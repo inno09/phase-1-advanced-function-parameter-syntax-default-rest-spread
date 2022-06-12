a42757e355684f18a0f7da13df2bf080f4cbc2c5
innocent <innonger@gmail.com>
2022-06-12 18:34:06 +0300

    commit tree 85b559433ba29bf892ca9dc61e9d4d6741eab148

---
+++ index.js
@@ -0,0 +1,37 @@
+const farmAnimals = 'cow horse sheep pig chicken';
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const muppet = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
+};
+
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+};
+
+const [ moo, neigh, baa, oink, cluck ] = farmAnimals.split(" ");
+const [ bessie, , dolly, babe, little] = farmAnimals.split(" ");
+const [blackAndWhite, , black, pink ] = farmAnimals.split(" ");
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg] = colors;
+const { muppetName, color, song, job, partner } = muppet;
+const { nestedJob, nestedPartner } = nestedMuppet;
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+
